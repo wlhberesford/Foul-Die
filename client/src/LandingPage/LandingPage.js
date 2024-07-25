9aafae568105d269a6b5ebc507327c24d43f9f95
Liam Beresford <wlhberesford@gmail.com>
2024-07-25 14:27:46 -0400

    Functional Landing Page

--- client/src/LandingPage/LandingPage.js
+++ client/src/LandingPage/LandingPage.js
@@ -7,18 +7,15 @@ import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { alpha, styled } from '@mui/material/styles';
 import { Stack } from '@mui/material';
-
-//import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
 
 
 const colors = createTheme({
     palette: {
-        
         clr_acc_dark: "#27374D",
         clr_bg_alt: "#526D82",
         clr_bg_main: "#9DB2BF",
         clr_acc_light: "#DDE6ED",
-        
       },
 });
 
@@ -34,19 +31,25 @@ const PartySwitch = styled(Switch)(({ theme }) => ({
     },
   }));
 
-
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: colors.palette.clr_acc_light,
+    backgroundColor: colors.palette.clr_acc_dark,
+    '&:hover': {
+        color: colors.palette.clr_acc_dark,
+        backgroundColor: colors.palette.clr_acc_light,
+    },
+  }));
 
 const EnterParty = () =>{
     return (
         <div className='enter-party'>
-            <div>
+            <Stack direction="column" spacing={2}>
                 <TextField id="party-code" label="Party Code" variant="outlined" />
 
                 <TextField id="name" label="Name" variant="outlined" />
 
-            </div>
-                <Button variant="text">Enter</Button>
-
+                <ColorButton variant="text">Enter</ColorButton>
+            </Stack>
         </div>
     );
 }
@@ -54,14 +57,31 @@ const EnterParty = () =>{
 const MakeParty = () =>{
     return (
         <div className='enter-party'>
-            <TextField id="party-code" label="Enter Party Code" variant="outlined" />
-            <TextField id="party-code" label="Enter Party Code" variant="outlined" />
-            <TextField id="party-code" label="Enter Party Code" variant="outlined" />
-            <TextField id="party-code" label="Enter Party Code" variant="outlined" />
-            <TextField id="party-code" label="Enter Party Code" variant="outlined" />
-            <TextField id="party-code" label="Enter Party Code" variant="outlined" />
+            <Stack direction="column" gap={2}>
+                <TextField id="name" label="Name" variant="outlined" />  
 
-
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={8}>
+                        <TextField id="punishment1" label="Punishment" variant="outlined" />  
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                        <TextField id="punishment2" label="Punishment" variant="outlined" />  
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                        <TextField id="punishment3" label="Punishment" variant="outlined" />  
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                        <TextField id="punishment4" label="Punishment" variant="outlined" />  
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                        <TextField id="punishment5" label="Punishment" variant="outlined" />  
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                        <TextField id="punishment6" label="Punishment" variant="outlined" />  
+                    </Grid>
+                </Grid>
+                <ColorButton variant="text">Enter</ColorButton>
+            </Stack>
         </div>
     );
 }
@@ -72,8 +92,6 @@ function changeForm(state){
     }
     return EnterParty;
 }
-
-
 
 function LandingPage() {
     const [newParty, SetNewParty]=useState(false);
